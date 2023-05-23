cf3cb761fe8f2b5765e3117fadb0cabb78355684
Vitalii Popovych <popovych@gmail.com>
2023-05-22 21:31:52 -0500

    initial setup

--- src/app.js
+++ src/app.js
@@ -1,20 +1,29 @@
+require('express-async-errors');
 const express = require('express');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
 const favicon = require('express-favicon');
 const logger = require('morgan');
-
+//routers
 const mainRouter = require('./routes/mainRouter.js');
+const petRouter = require('./routes/petRoutes.js');
 
 // middleware
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
-app.use(express.static('public'))
+app.use(express.static('public'));
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
-// routes
+// routes middleware
 app.use('/api/v1', mainRouter);
+app.use('/api/v1/pets', petRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,19 @@
+require('dotenv').config();
 const { PORT = 8000 } = process.env;
-const app = require("./app");
+const app = require('./app');
+
+//database
+const connectDB = require('./db/connect');
 
 const listener = () => console.log(`Listening on Port ${PORT}!`);
-app.listen(PORT, listener);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, listener);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
