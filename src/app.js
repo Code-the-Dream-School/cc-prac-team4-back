311a0b9c3696b1d2c4f2595c6b1fbcbfd9ac37f6
Nata Kalina <natakalinatester@gmail.com>
2023-05-26 17:35:12 -0400

    User module changed main setup

--- src/app.js
+++ src/app.js
@@ -6,9 +6,13 @@ const logger = require("morgan");
 
 require("express-async-errors");
 require("dotenv").config();
+const cookieParser = require("cookie-parser");
+
+//app
 
 const mainRouter = require("./routes/mainRouter.js");
 const userRouter = require("./routes/userRouter.js");
+const authRouter = require("./routes/authRoter.js");
 
 const errorHandlerMiddleware = require("../middleware/error-handler.js");
 
@@ -19,12 +23,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(logger("dev"));
 app.use(express.static("public"));
 app.use(favicon(__dirname + "/public/favicon.ico"));
-
+app.use(cookieParser());
 
 app.use(errorHandlerMiddleware);
 
 // routes
 app.use("/api/v1", mainRouter);
 app.use("/api/v1", userRouter);
+app.use("/api/v1", authRouter);
 
 module.exports = app;
